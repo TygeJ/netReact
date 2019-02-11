72f5b3d14d19d80b8a89d9f5a5314dab4c8f73da
Tyge Johnson <tygejohnson@Tyge-Johnsons-MacBook-Pro.local>
2019-02-11 12:36:21 -0700

    index change

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-alert("test");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ToDo = React.createClass({
+  render: function(){
+    return(
+      <h1>some text</h1>
+    );
+  }
+});
+
+ReactDOM.render(<ToDoComponent />, document.getElementById('todo-wrapper'));
