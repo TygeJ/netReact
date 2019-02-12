a43aa30741521f65dcab664d662066b9cde7412e
Tyge Johnson <tygejohnson@Tyge-Johnsons-MacBook-Pro.local>
2019-02-12 08:25:38 -0700

    clear out

--- src/app/index.js
+++ src/app/index.js
@@ -5,14 +5,11 @@ const ToDoComponent = React.createClass({
   render: function(){
     return(
       <div>
-        <p>Car Name: {this.props.car.name}</p>
-        <p>Car color: {this.props.car.color}</p>
-        <p>Car engine: {this.props.car.engine}</p>
+
       </div>
     );
   }
 });
 
-const myCar ={name: 'Toyota', color: 'red', engine: 'v6'}
 
-ReactDOM.render(<ToDoComponent  mssg="test 2" car={myCar} />, document.getElementById('todo-wrapper'));
+ReactDOM.render(<ToDoComponent   />, document.getElementById('todo-wrapper'));
