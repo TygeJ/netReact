06c5777118eb78fea9fce4a39268561882f2fc8d
Tyge Johnson <tygejohnson@Tyge-Johnsons-MacBook-Pro.local>
2019-02-12 08:37:55 -0700

    add state

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,23 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 const ToDoComponent = React.createClass({
+  getInitialState:  function(){
+    return {
+      todos: ['eat', 'sleep', 'work']
+    }
+  },
   render: function(){
     return(
-      <div>
-
+      <div id="todo-list">
+        <p>todo list text</p>
+        <ul>
+          <li>{this.state.todos[0]}</li>
+          <li>{this.state.todos[1]}</li>
+          <li>{this.state.todos[2]}</li>
+        </ul>
       </div>
     );
-  }
+  } //render
 });
 
 
