b3448d4235786aac5462d915cc4fe26ae73a8742
Tyge Johnson <tygejohnson@Tyge-Johnsons-MacBook-Pro.local>
2019-02-13 13:48:07 -0700

    fix todos state

--- src/app/index.js
+++ src/app/index.js
@@ -4,22 +4,25 @@ const ReactDOM = require('react-dom');
 const ToDoComponent = React.createClass({
   getInitialState:  function(){
     return {
-      todos: ['eat', 'sleep', 'work']
+      todos: ['eat', 'sleep', 'work'],
     }
   },
   render: function(){
+    le
+     todos = this.state.todos;
+    todos = todos.map(function(item, index){
+      return(
+        <li>{item}</li>
+      );
+    });
     return(
       <div id="todo-list">
-        <p>todo list text</p>
-        <ul>
-          <li>{this.state.todos[0]}</li>
-          <li>{this.state.todos[1]}</li>
-          <li>{this.state.todos[2]}</li>
-        </ul>
+        <p>text 1</p>
+        <ul>{todos}</ul>
       </div>
-    );
-  } //render
+    )
+  }
 });
 
 
-ReactDOM.render(<ToDoComponent   />, document.getElementById('todo-wrapper'));
+ReactDOM.render(<ToDoComponent />, document.getElementById('todo-wrapper'));
